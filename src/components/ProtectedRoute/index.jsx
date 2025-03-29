daf08449cd58e2604fa2533f49e730577d2ccf5a
Minh Dinh <macotontai123@gmail.com>
2025-03-30 00:04:07 +0700

    feat(auth): add `isLoaded` to authContext and ProtectedRoute

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -2,10 +2,12 @@ import useAuth from "@/hooks/useAuth";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const ProtectedRoute = ({ allowedRoles }) => {
-  const { user } = useAuth();
+  const { user, isLoaded } = useAuth();
   const location = useLocation();
 
   console.log("User", user);
+
+  if (!isLoaded) return null;
 
   if (!user) return <Navigate to="/login" state={{ from: location }} replace />;
 
